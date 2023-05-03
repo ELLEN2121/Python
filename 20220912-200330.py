60810ea16c314f9cde73744ce25439c87b221b6f
Ellen Maria <61208054+ELLEN2121@users.noreply.github.com>
2023-05-03 19:11:59 -0300

    Add files via upload
    Python aula

---
+++ 20220912-200330.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+number1 = int(input('Entre com o primeiro número: '))
+number2 = int(input('Entre com o segundo número: '))
+print("Soma")
+print(number1 + number2)
+print("Multiplação")
+print(number1*number2)
+print("Subtração")
+print(number1-number2)
+print("divisão")
+print(number1/number2)
---
+++ 20230417-183301.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+valor = [1,2,3]
+print(sum(valor))
+print(min(valor))
+print(max(valor))
